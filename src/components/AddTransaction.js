2b0f6ebcdda8e462109f2b607cfc184877924689
cparros <celparros@gmail.com>
2021-06-01 18:21:24 -0400

    changed some text and removed dumy transactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -23,12 +23,12 @@ export const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit} id="form">
         <div className="form-control">
-          <label htmlFor="text">Text</label>
+          <label htmlFor="text">Transaction</label>
           <input
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder="Enter a transaction title..."
           />
         </div>
         <div className="form-control">
